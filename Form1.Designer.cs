51f0661ce1a9d2b01fd2cfc7ffca021fb2b14bf8
baikangwang <baikangwang@hotmail.com>
2016-10-24 16:15:40 +0800

    Initial check in

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace AeronetChartSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(0, 49);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(488, 373);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2016"});
+            this.comboBox1.Location = new System.Drawing.Point(38, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(53, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "01",
+            "02",
+            "03",
+            "04",
+            "05",
+            "06",
+            "07",
+            "08",
+            "09",
+            "10",
+            "11",
+            "12"});
+            this.comboBox2.Location = new System.Drawing.Point(156, 12);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(52, 21);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "01",
+            "02",
+            "03",
+            "04",
+            "05",
+            "06",
+            "07",
+            "08",
+            "09",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31"});
+            this.comboBox3.Location = new System.Drawing.Point(267, 12);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(57, 21);
+            this.comboBox3.TabIndex = 3;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Year";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(113, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Month";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(235, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Day";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 422);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace AeronetChartSample
+{
+    public partial class Form1 : Form
+    {
+        private IDictionary<string, IDictionary<string, dynamic[]>> _data;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ChartArea caDefault = new ChartArea("Default");
+            this.chart1.ChartAreas.Clear();
+            this.chart1.ChartAreas.Add(caDefault);
+            Legend lgDefault = new Legend("Default");
+            this.chart1.Legends.Clear();
+            this.chart1.Legends.Add(lgDefault);
+            this.chart1.Series.Clear();
+            this.chart1.Titles.Clear();
+
+            InitData();
+
+            this.comboBox1.SelectedIndex = 0;
+            this.comboBox2.SelectedIndex = 1;
+            this.Text = "Aeronet Chart Sample";
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var combox = sender as ComboBox;
+            var day = combox.Text;
+            //validate
+            if (!this._data.ContainsKey(day))
+            {
+                MessageBox.Show("No Available data");
+                return;
+            }
+
+            // load data
+            var data = this._data[day];
+
+            // add title
+            var year = comboBox1.Text;
+            var month = comboBox2.Text;
+            this.chart1.Titles.Clear();
+            this.chart1.Titles.Add(new Title(string.Format("{0} / {1} / {2}", month, day, year), Docking.Top)
+            {
+                DockedToChartArea = "Default",
+                IsDockedInsideChartArea = false
+            });
+
+            // add series
+            this.chart1.Series.Clear();
+            foreach (string time in data.Keys)
+            {
+                Series timeLine = new Series(time);
+                timeLine.Legend = "Default";
+                timeLine.ChartType = SeriesChartType.Line;
+                timeLine.ChartArea = "Default";
+                var points = data[time];
+                foreach (var point in points)
+                {
+                    timeLine.Points.Add(new DataPoint(point.X, point.Y));
+                }
+                this.chart1.Series.Add(timeLine);
+            }
+        }
+
+        private void InitData()
+        {
+            this._data = new Dictionary<string, IDictionary<string, dynamic[]>>();
+            this._data.Add(
+                "01", new Dictionary<string, dynamic[]>()
+                {
+                    {"08:12:33",new dynamic[]{new{X=400,Y=60.88},new {X=550,Y=58.33},new {X=600,Y=43.56},new {X=720,Y=40.12},new {X=880,Y=30.22}}},
+                    {"13:55:04",new dynamic[]{new{X=400,Y=70.78},new {X=550,Y=68.33},new {X=600,Y=53.56},new {X=720,Y=50.12},new {X=880,Y=48.22}}},
+                    {"21:01:01",new dynamic[]{new{X=400,Y=50.11},new {X=550,Y=45.33},new {X=600,Y=32.56},new {X=720,Y=31.12},new {X=880,Y=20.22}}}
+                });
+            this._data.Add(
+                "02", new Dictionary<string, dynamic[]>()
+                {
+                    {"04:22:16",new dynamic[]{new{X=400,Y=90.88},new {X=550,Y=70.33},new {X=600,Y=66.56},new {X=720,Y=65.12},new {X=880,Y=40.22}}},
+                    {"17:35:02",new dynamic[]{new{X=400,Y=88.78},new {X=550,Y=71.33},new {X=600,Y=61.56},new {X=720,Y=65.12},new {X=880,Y=54.22}}},
+                });
+            this._data.Add(
+                "03", new Dictionary<string, dynamic[]>()
+                {
+                    {"02:38:16",new dynamic[]{new{X=400,Y=90.88},new {X=550,Y=89.33},new {X=600,Y=72.56},new {X=720,Y=65.12},new {X=880,Y=31.22}}},
+                    {"08:02:55",new dynamic[]{new{X=400,Y=63.78},new {X=550,Y=71.33},new {X=600,Y=37.56},new {X=720,Y=56.12},new {X=880,Y=44.22}}},
+                    {"12:33:11",new dynamic[]{new{X=400,Y=78.88},new {X=550,Y=70.33},new {X=600,Y=65.56},new {X=720,Y=60.12},new {X=880,Y=40.22}}},
+                    {"20:04:24",new dynamic[]{new{X=400,Y=80.78},new {X=550,Y=75.33},new {X=600,Y=68.56},new {X=720,Y=70.12},new {X=880,Y=54.22}}},
+                });
+        }
+    }
+}
